014d3d3ef1c2faa6a54836363b5b391ddf4114cb
Julio Berina <julioberina@gmail.com>
2018-05-05 16:57:28 -0700

    Implemented standard matrix multiplication

--- main.cpp
+++ main.cpp
@@ -11,3 +11,28 @@ int main()
     cout << "Program coming soon..." << endl;
     return 0;
 }
+
+matrix multiply(const matrix& m1, const matrix& m2)
+{
+    int n = m1.getSize();
+    int* rowVector = nullptr;
+
+    matrix result(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        rowVector = new int[n];
+
+        for (int j = 0; j < n; ++j)
+        {
+            rowVector[j] = 0;
+
+            for (int k = 0; k < n; ++k)
+                rowVector[j] += (m1.getMatrixValue(i, k) * m2.getMatrixValue(k, j));
+        }
+
+        result.insertRow(i, rowVector);
+    }
+
+    return result;
+}
